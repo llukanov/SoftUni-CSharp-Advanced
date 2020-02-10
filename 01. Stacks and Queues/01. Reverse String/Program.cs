480d75fb0ae6346c7540aa018b1664081569bd82
llukanov <60892930+llukanov@users.noreply.github.com>
2020-02-10 23:15:11 +0200

    add solutions

---
+++ 01. Stacks and Queues/01. Reverse String/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Reverse_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var text = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                text.Push(input[i]);
+            }
+
+            while (text.Any())
+            {
+                Console.Write(text.Pop());
+            }
+        }
+    }
+}
---
+++ 01. Stacks and Queues/02. Stack Sum/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Stack_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            Stack<int> stack = new Stack<int>(numbers);
+            string commandCurent = Console.ReadLine().ToLower();
+
+            while (true)
+            {
+                string[] commands = Console.ReadLine().ToLower().Split();
+
+                if (commands[0] == "add")
+                {
+                    stack.Push(int.Parse(commands[1]));
+                    stack.Push(int.Parse(commands[2]));
+                }
+                else if (commands[0] == "remove")
+                {
+                    int n = int.Parse(commands[1]);
+
+                    if (n >= stack.Count)
+                    {
+                        for (int i = 0; i < n; i++)
+                        {
+                            stack.Pop();
+                        }
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine($"Sum: {stack.Sum()}");
+        }
+    }
+}
