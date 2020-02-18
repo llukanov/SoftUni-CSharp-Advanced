62f1dd4b3672bc6c348bab4c357e6d53d7a3fd69
llukanov <60892930+llukanov@users.noreply.github.com>
2020-02-18 21:41:58 +0200

    defining classes solutions

---
+++ 11. Defining Classes/CarEngineAndTires/Engine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarEngineAndTires
+{
+    public class Engine
+    {
+        private int horsePower;
+        private double cubicCapacity;
+
+        public Engine(int horsePower, double cubicCapacity)
+        {
+            this.HorsePower = horsePower;
+            this.CubicCapacity = cubicCapacity;
+        }
+        public int HorsePower { get; set; }
+        public double CubicCapacity { get; set; }
+    }
+}
---
+++ 11. Defining Classes/CarExtension/Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double FuelQuantity { get; set; }
+        public double FuelConsumption { get; set; }
+        public void Drive(double distance)
+        {
+            if (FuelQuantity - (distance / 100 * FuelConsumption) > 0)
+            {
+                FuelQuantity -= distance / 100 * FuelConsumption;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+        public string WhoAmI()
+        {
+            StringBuilder result = new StringBuilder();
+            result.AppendLine($"Make: {this.Make}");
+            result.AppendLine($"Model: {this.Model}");
+            result.AppendLine($"Year: {this.Year}");
+            result.AppendLine($"Fuel: {this.FuelQuantity:F2}L");
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}
