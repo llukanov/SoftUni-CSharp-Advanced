689c37ea40b88cb9edd58c94bde43f9a71ae83e8
llukanov <60892930+llukanov@users.noreply.github.com>
2020-02-20 13:48:35 +0200

    new solution

---
+++ 19. Exam Preparation/Advanced-Retake-Exam-13-Aug-2019/03.AquariumAdventure/Aquarium.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AquariumAdventure
+{
+    class Aquarium
+    {
+        private readonly List<Fish> fishInPool;
+
+        private Aquarium()
+        {
+            this.fishInPool = new List<Fish>();
+        }
+
+        public Aquarium(string name, int capacity, int size)
+            : this()
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.Size = size;
+        }
+
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int Size { get; set; }
+
+        public void Add(Fish fish)
+        {
+            if (this.fishInPool.Count + 1 <= this.Capacity
+                && this.fishInPool.FirstOrDefault(r => r == fish) == null)
+            {
+                this.fishInPool.Add(fish);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            Fish fish = this.fishInPool
+                .FirstOrDefault(r => r.Name == name);
+
+            if (fish != null)
+            {
+                this.fishInPool.Remove(fish);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Fish FindFish(string name)
+        {
+            Fish fish = this.fishInPool
+                .FirstOrDefault(r => r.Name == name);
+
+            if (fish != null)
+            {
+                return fish;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Aquarium: {this.Name} ^ Size: {this.Size}");
+
+            foreach (var fish in fishInPool)
+            {
+                sb.AppendLine($"{fish.ToString()}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 19. Exam Preparation/AdvancedExam-23-Jun-2019/03.SpaceStationEstablishment/StartUp.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Text;
+
+namespace _03.SpaceStationEstablishment
+{
+    class StartUp
+    {
+        static char[,] matrix;
+        static int spaceshipRow;
+        static int spaceshipCol;
+        static StringBuilder @string;
+
+        public static void Main()
+        {
+            int energy = 0;
+
+            @string = new StringBuilder();
+
+            string initialString = Console.ReadLine();
+
+            int matrixSize = int.Parse(Console.ReadLine());
+            matrix = new char[matrixSize, matrixSize];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string input = Console.ReadLine();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+
+                    if (matrix[row, col] == 'S')
+                    {
+                        spaceshipRow = row;
+                        spaceshipCol = col;
+                    }
+                }
+            }
+
+            @string.Append(initialString);
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+                switch (command)
+                {
+                    case "up":
+                        Move(-1, 0, ref energy);
+                        break;
+                    case "down":
+                        Move(1, 0, ref energy);
+                        break;
+                    case "left":
+                        Move(0, -1, ref energy); 
+                        break;
+                    case "right":
+                        Move(0, 1, energy);
+                        break;
+                }
+            }
+
+            PrintMatrix();
+        }
+
+        private static void PrintMatrix()
+        {
+            Console.WriteLine(@string.ToString());
+            matrix[spaceshipRow, spaceshipCol] = 'P';
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static void Move(int row, int col, ref energy)
+        {
+            if (spaceshipRow + row >= 0
+                && spaceshipRow + row < matrix.GetLength(0)
+                && spaceshipCol + col >= 0
+                && spaceshipCol + col < matrix.GetLength(1))
+            {
+                if (matrix[spaceshipRow, spaceshipCol] == 'S')
+                {
+                    matrix[spaceshipRow, spaceshipCol] = '-';
+                }
+
+                spaceshipRow += row;
+                spaceshipCol += col;
+
+                char cell = matrix[spaceshipRow, spaceshipCol];
+
+                if (char.IsDigit(cell))
+                {
+                    energy =
+                    @string.Append(cell);
+                    matrix[spaceshipRow, spaceshipCol] = '-';
+                }
+
+                matrix[spaceshipRow, spaceshipCol] = 'P';
+            }
+            else
+            {
+                @string.Remove(@string.Length - 1, 1);
+            }
+        }
+
+        private static bool IsValid(int row, int col)
+        {
+            return spaceshipRow + row >= 0 && spaceshipRow + row < matrix.GetLength(0) &&
+                   spaceshipCol + col >= 0 && spaceshipCol + col < matrix.GetLength(1);
+        }
+    }
+}
